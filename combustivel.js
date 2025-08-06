23a899e5a90860b7aabe39e12112b057b49ecf65
João Vitor <joao.tavares@pdcase.com.br>
2025-08-06 16:20:58 -0300

    Atualização do exercício de Javascript

--- combustivel.js
+++ combustivel.js
@@ -2,10 +2,10 @@ let literKm, tripKm, ValueLiter;
 
 literKm = 10;
 tripKm = 250;
-ValueLiter = 5.79
+ValueLiter = 5.79;
 
-let literNecessary = tripKm/literKm;
-let literCost = literNecessary * ValueLiter;
+const literNecessary = tripKm/literKm;
+const literCost = literNecessary * ValueLiter;
 
 console.log("Serão necessários: ", literNecessary, "litros")
 console.log("o Custo do litro é: ", literCost, "reais")
--- retangulo.js
+++ retangulo.js
@@ -3,7 +3,7 @@ let altura, base;
 altura = 6;
 base = 4;
 
-let = retangulo = altura * base;
+const retangulo = altura * base;
 
 console.log("O calculo de base e altura do retangulo é:", retangulo,);
 console.log("A altura do rentagulo é:", altura)
