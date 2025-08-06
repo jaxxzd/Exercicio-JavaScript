f441da0f15ae2ba9581fa8ab05b11ce58586e459
João Vitor <joao.tavares@pdcase.com.br>
2025-08-06 17:55:53 -0300

    feito a atualização no exercício de Javascript

--- calcIMC.js
+++ calcIMC.js
@@ -9,7 +9,7 @@ if(IMC >= 30) {
     console.log("Você está com o IMC de:", IMC, "e está classificado como obeso")
 }
 
-else if(IMC >= 25){
+else if(IMC >= 25 && IMC < 30){
     console.log("Você está com o IMC de:", IMC, "e está classificado como sobrepeso")
 }
 
