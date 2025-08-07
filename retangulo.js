ff3f1ac167a4b050228e11ada66c081ae0f03a36
João Vitor <joao.tavares@pdcase.com.br>
2025-08-07 14:43:54 -0300

    Atualização do arquivo retangulo.js

--- retangulo.js
+++ retangulo.js
@@ -3,8 +3,8 @@ let altura, base;
 altura = 6;
 base = 4;
 
-const retangulo = altura * base;
+const area = altura * base;
 
-console.log("O calculo de base e altura do retangulo é:", retangulo,);
+console.log("O calculo de base e altura do retangulo é:", area,);
 console.log("A altura do rentagulo é:", altura)
 console.log("A base do retangulo é:", base)
