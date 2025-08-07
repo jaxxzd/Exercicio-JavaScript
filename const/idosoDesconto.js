28804181325b5f1f30ab1a65e95a3ce0697e7c0a
João Vitor <joao.tavares@pdcase.com.br>
2025-08-07 17:18:08 -0300

    Atualização no arquivo idosoDesconto.js do exercício de javaScript

--- const/idosoDesconto.js
+++ const/idosoDesconto.js
@@ -1,14 +1,14 @@
 let valueTicket, age; 
 
 valueTicket = 40;
-age = 60;
+age = 59;
 
 const ElderlyRequirements = {Discount: 0.5, ageMinimum: 60};
 
 if(age >= ElderlyRequirements.ageMinimum){
     let valueFinal = valueTicket * ElderlyRequirements.Discount
-    console.log("Você recebeu o Desconto de 50%, valor final foi aplicado para:", valueFinal)
+    console.log("Você recebeu o Desconto de 50%, valor final foi aplicado para:", "R$", valueFinal.toFixed(2))
 }
 else{
-    console.log("Você não recebeu desconto, valor final foi aplicado para:", valueTicket)
+    console.log("Você não recebeu desconto, valor final foi aplicado para:", "R$", valueTicket.toFixed(2))
 }
