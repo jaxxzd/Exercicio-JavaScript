8705cee6668d84042ceb6bf251ae4b84d96ed416
João Vitor <joao.tavares@pdcase.com.br>
2025-08-25 16:03:18 -0300

    Atualização do Live Code em JavaScript

--- LiveCode/boletimProvaFinal.js
+++ LiveCode/boletimProvaFinal.js
@@ -1,14 +1,15 @@
 const prompt = require('prompt-sync')();
 
-let name, P1, P2, P3, frequency, bonusParticipation, PF, MP, MF, absence, lowGrade, result;
+let name, P1, P2, P3, frequency, bonusParticipation, MP, PF, MF, absence, lowGrade, result;
 
 name = prompt("Digite o nome do aluno: ");
-P1 = Math.round(parseFloat(prompt("Digita a primeira nota (0 a 10): ")));
-P2 = Math.round(parseFloat(prompt("Digita a segunda nota (0 a 10): ")));
-P3 = Math.round(parseFloat(prompt("Digita a terceira nota (0 a 10): ")));
-bonusParticipation = parseInt(prompt("Digite o bônus de participação (0 a 1): ")) === 1 ? +1 : 0;
+P1 = Math.round(parseFloat(prompt("Digite a primeira nota (0 a 10): ")));
+P2 = Math.round(parseFloat(prompt("Digite a segunda nota (0 a 10): ")));
+P3 = Math.round(parseFloat(prompt("Digite a terceira nota (0 a 10): ")));
+bonusParticipation = parseInt(prompt("Digite o bônus de participação (0 a 1): "));
 MP = P1 * 0.3 + P2 * 0.3 + P3 * 0.4;
-(MP += bonusParticipation > 10 ? 10 : 0).toFixed(1);
+MP = parseFloat(MP.toFixed(1));
+MF = null;
 PF = parseFloat(prompt("Digite a nota da Prova Final (0 a 10): "));
 frequency = parseInt(prompt("Digite a frequência do aluno (1 a 100): "));
 
@@ -16,37 +17,46 @@ console.log(" ")
 
 // Média parcial (MP)
 
-if(isNaN(P1) || P1 < 0 || P1 > 100 || isNaN(P2) || P2 < 0 || P2 > 100 || isNaN(P3) || P3 < 0 || P3 > 100) {
-    console.error("As notas devem ser informadas entre 0 a 100");
+if (isNaN(P1) || P1 < 0 || P1 > 10 || isNaN(P2) || P2 < 0 || P2 > 10 || isNaN(P3) || P3 < 0 || P3 > 10) {
+    console.error("As notas devem ser informadas entre 0 a 10");
     process.exit();
 }
 
 console.log(" ")
 
-// Regras de aprovação
+// Bônus de participação
 
-if(frequency < 75) {
-    console.error(`Reprovado por falta (${frequency})`);
+if (bonusParticipation == 1 && MP == 10) {
+    MP += bonusParticipation;
+    if (MP > 10) {
+        MP = 10;
+    }
 }
 
-if(MP >= 7.0) {
+// Regras de aprovação
+
+if (frequency < 75) {
+    result = `Reprovado por falta (${frequency})`;
+}
+
+if (MP >= 7) {
     result = "Aprovado Direto";
 }
 
-else if(frequency > 75 && 4.0 <= MP < 7.0) {
-    console.log("Recuperação(Prova Final)")
+else if (frequency > 75 && MP >= 4.0 && MP < 7.0) {
+    result = "Recuperação(Prova Final)";
+
+    MF = (MP + PF) / 2;
+
+    if (MF >= 5.0) {
+        result = "Aprovado por final";
+    }
+    else {
+        result = "Reprovado por nota"
+    }
 }
 
-MF = (MP + PF) / 2;
-
-if(MF >= 5.0) {
-    result = "Aprovado por final";
-}
-else{
-    result = "Reprovado por nota"
-}
-
-if(MP < 4.0) {
+else if (MP < 4.0) {
     result = "Reprovado por nota (sem direito a Prova Final)";
 }
 
@@ -54,8 +64,8 @@ console.log(" ")
 
 // Regra extra de segurança
 
-if(P1, P2, P3 < 2 && MF >= 6.0) {
-    result = "Aprovado por final"
+if ((P1 < 2 || P2 < 2 || P3 < 2) && MF >= 6.0) {
+    result = "Aprovado por final";
 }
 
 // Saída
@@ -70,10 +80,10 @@ console.log("nome do aluno é:", name);
 console.log("P1:", P1);
 console.log("P2:", P2);
 console.log("P3:", P3);
-console.log("Frequência:", "%", frequency);
+console.log("Frequência:", frequency + "%");
 console.log("Bônus aplicado:", bonusParticipation);
-console.log("Mèdia parcial após o bônus:", Math.round(MP));
-console.log("Nota da prova final é:", Math.round(PF));
-console.log("Mèdia Final é:", Math.round(MF));
-console.log("A situação do aluno é:", result);
+console.log("Média parcial após o bônus:", MP);
+console.log("Nota da prova final:", Math.round(PF));
+console.log("Média Final:", MF != null ? Math.round(MF) : "Não se aplica");
+console.log("Situação do aluno:", result);
 
