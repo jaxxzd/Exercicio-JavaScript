2f0befd987f99552d92b918339bb17dab704d812
João Vitor <joao.tavares@pdcase.com.br>
2025-08-25 18:20:40 -0300

    Atualização do css da atividade incorporando JavaScript no HTML

--- JavaScriptHTML/paginaVerificacao.js
+++ JavaScriptHTML/paginaVerificacao.js
@@ -2,20 +2,18 @@ function Verification() {
     
     let nome, idade, result;
 
-    nome = document.getElementById("name").value
+    nome = document.getElementById("name").value;
     idade = document.getElementById("age").value
     result = "";
 
-    if (nome == "") {
-        nome = "Visitante!"
-    }
+    nome = nome === " " ? "Visitante" : nome;
 
-    if (age >= 18) {
+    if (idade >= 18) {
         result = `Olá, ${nome}, você é maior de idade!`;
     }
     else {
         result = `Olá, ${nome}, você é menor de idade!`;
     }
-}
 
-document.getElementById("result").innerHTML = result;
+    document.getElementById("result").innerHTML = result;
+}
