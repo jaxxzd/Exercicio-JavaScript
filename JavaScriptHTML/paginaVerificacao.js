521f4dff2a45cdac822617039a9d75674dddeb58
João Vitor <joao.tavares@pdcase.com.br>
2025-08-26 15:16:51 -0300

    Atualização da atividade sobre incorporação do JavaScript no HTML

--- JavaScriptHTML/paginaVerificacao.js
+++ JavaScriptHTML/paginaVerificacao.js
@@ -3,16 +3,35 @@ function Verification() {
     let nome, idade, result;
 
     nome = document.getElementById("name").value;
-    idade = document.getElementById("age").value
+    idade = document.getElementById("age").value;
     result = "";
 
-    nome = nome === " " ? "Visitante" : nome;
+    idade = parseInt(idade)
+    console.log(typeof(idade))
 
-    if (idade >= 18) {
+    // nome
+
+    if (nome === "") {
+        nome = "Visitante"
+    }
+    // idade
+    
+    if (idade >= 18 && idade < 122) {
         result = `Olá, ${nome}, você é maior de idade!`;
     }
-    else {
+    else if (idade == "") {
+
+    }
+    else if (idade < 18 && idade >= 0) {
         result = `Olá, ${nome}, você é menor de idade!`;
+    }
+    else if (idade < 0 || idade > 122 || isNaN(idade)) {
+        result = "Idade inválida";
+        document.getElementById("result").style.color = "red";
+    }
+    else {
+        result = "idade inválida";
+        document.getElementById("result").style.color = "red";
     }
 
     document.getElementById("result").innerHTML = result;
