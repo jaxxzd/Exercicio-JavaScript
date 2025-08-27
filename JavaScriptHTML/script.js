a4786df88c8277131499d99a652f19eb170b55d6
João Vitor <joao.tavares@pdcase.com.br>
2025-08-27 19:11:41 -0300

    Código atualizado para melhor versão sobre a atividade com relação de JavaScript com HTML

--- JavaScriptHTML/script.js
+++ JavaScriptHTML/script.js
@@ -1,40 +1,62 @@
+let timeClearing = null;
+
 function verifyGlacier() {
     let name, quantityIceCream, valueTotal, result;
 
-    name = document.getElementById("input-name-glacier").value;
-    quantityIceCream = document.getElementById("input-quantity-ice-cream-glacier").value;
-    result = "";
+    name = document.getElementById("input-name-glacier").value.trim();
+    quantityIceCream = Number(document.getElementById("input-quantity-ice-cream-glacier").value.trim());
 
     const valueIceCream = 8;
+
+    const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
 
     valueTotal = valueIceCream * quantityIceCream;
 
     // nome
 
-    if(name === "") {
+    if (name === "") {
         name = "Visitante";
     }
-    else if (name === Number) {
-        name = "Visitante";
+    else if (!nameRegex.test(name)) {
+        showResult("Erro: nome inválido", "red");
+        return
     }
 
     // quantidade de sorvetes
 
-    if(quantityIceCream > 0 && quantityIceCream !== NaN) {
-        result = `Olá, ${name}, você comprou ${quantityIceCream} sorvetes, o valor debitado é ${"R$ " + valueTotal.toFixed(2)}`;
+    if (quantityIceCream > 1) {
+        showResult(`Olá, ${name}, você comprou ${quantityIceCream} sorvetes, o valor debitado é ${"R$ " + valueTotal.toFixed(2)}`, "green");
     }
-    else if (quantityIceCream === "") {
-        
+    else if (quantityIceCream === 1) {
+        showResult(`Olá, ${name}, você comprou ${quantityIceCream} sorvete, o valor debitado é ${"R$ " + valueTotal.
+        toFixed(2)}`, "green");
     }
-    else if(quantityIceCream === NaN) {
-        result = `Digite uma quantidade válida`
-        document.getElementById("result").style.color = "red";
-    }
+    else if (quantityIceCream <= 0 || isNaN(quantityIceCream))
     else {
-        result = `Digite uma quantidade válida`
-        document.getElementById("result").style.color = "red";
+        showResult("Digite uma quantidade válida", "red")
     }
 
+    // limpeza dos inputs
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("input-name-glacier").value = "";
+    document.getElementById("input-quantity-ice-cream-glacier").value = "";
+}
+
+function showResult(message, color = "green") {
+    // execução do programa
+
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = message;
+    resultElement.style.color = color;
+    resultElement.style.opacity = 1
+
+    // cancela tempos anteriores
+
+    if(timeClearing) clearTimeout(timeClearing);
+
+    // limpeza da mensagem
+
+    timeClearing = setTimeout(() => {
+        resultElement.style.opacity = 0;
+    }, 3500);
 }
