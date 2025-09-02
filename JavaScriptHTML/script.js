553e473eded8dd376e1e8bfc692118f91bac9b6e
João Vitor <joao.tavares@pdcase.com.br>
2025-09-02 17:57:01 -0300

    Atualização de redução de código e simplificação da atividade JavaScript + HTML

--- JavaScriptHTML/script.js
+++ JavaScriptHTML/script.js
@@ -1,47 +1,43 @@
 function verifyGlacier() {
     let name, quantityIceCream, valueTotal, error;
 
-    name = document.getElementById("input-name-glacier").value.trim();
-    quantityIceCream = (document.getElementById("input-quantity-ice-cream-glacier").value.trim());
-    error = {};
-
-    // verificação de ambos inputs vazios
-
-    if (!name && quantityIceCream.trim() === "") {
-        return
-    };
-
+    name = document.getElementById("input-name-glacier").value;
+    quantityIceCream = document.getElementById("input-quantity-ice-cream-glacier").value;
     const valueIceCream = 8;
     const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
+    error = {};
+    
+    // verificação de ambos inputs vazios
+
+    if (!name.trim() && !quantityIceCream) return;
 
     // nome
 
-    if (name === "") {
-        name = "Visitante";
-    }
+    name = name.trim() || "Visitante";
+
     if (!nameRegex.test(name)) {
         error.nome = "✕ erro: Nome inválido";
     } 
 
     // validação de erros da quantidade de sorvetes
 
-    if (quantityIceCream === "") {
+    if (!quantityIceCream) {
         error.quantidade = "✕ erro: Campo de quantidade obrigatório";
     } else {
         quantityIceCream = Number(quantityIceCream);
-        if (quantityIceCream <= 0 || isNaN(quantityIceCream)) {
+        if (quantityIceCream <= 0) {
             error.quantidade ="✕ erro: Quantidade inválida";
         }
     }
 
-    // detectação de erro de ambos os inputs
+    // detectação de erro de ambos os inputs e aparecendo juntos se houver
 
-    if (Object.keys(error).length > 0) {
+    if (Object.keys(error).length) {
         showResult(Object.values(error).join("<br>"), "red");
         return
     }
 
-    const caseSensitive = quantityIceCream === 1 ? "sorvete" : "sorvetes";
+    const caseSensitive = quantityIceCream == 1 ? "sorvete" : "sorvetes";
     valueTotal = valueIceCream * quantityIceCream;
 
     showResult(`✓ Olá, ${name}, você comprou ${quantityIceCream} ${caseSensitive}, o valor debitado é ${"R$ " + valueTotal.toFixed(2)}`, "green");
